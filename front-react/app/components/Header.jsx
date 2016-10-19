832bf98ab144b8282be134dfcad852e28943beff
Jitang Zheng <jitang.zheng@gmail.com>
2016-10-19 11:46:42 -0700

    header bar

--- front-react/app/components/Header.jsx
+++ front-react/app/components/Header.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
 
 import SwitchLang from './SwitchLang'
 import PersonalBar from './PersonalBar'
 
-const Widget = () => (
+const Widget = ({info}) => (
   <Navbar inverse fixedTop fluid>
     <Navbar.Header>
       <Navbar.Brand>
-        <a href="#">site.sub_title</a>
+        <a href="/">{info.subTitle}</a>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav>
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
+        {info.bottomLinks.map(function (l, i) {
+          return (<NavItem eventKey={i} key={i} href={l.href}>
+            {l.label}
+          </NavItem>)
+        })}
         <SwitchLang />
       </Nav>
       <Nav pullRight>
-        <NavItem eventKey={1} href="#">Link Right</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
         <PersonalBar/>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
 )
 
-export default Widget
+Widget.propTypes = {
+  info: PropTypes.object.isRequired
+}
+
+export default connect(
+  state => ({info: state.siteInfo}),
+  dispatch => ({})
+)(Widget)
