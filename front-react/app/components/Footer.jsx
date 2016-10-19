4b8ea286cc674544383881fc78c01731877822cb
Jitang Zheng <jitang.zheng@gmail.com>
2016-10-19 11:07:06 -0700

    redux with i18next

--- front-react/app/components/Footer.jsx
+++ front-react/app/components/Footer.jsx
@@ -1,29 +1,56 @@
 import React, { PropTypes } from 'react'
 import { translate } from 'react-i18next'
+import { connect } from 'react-redux'
 
-const Widget = ({t}) => (
-  <div className="row">
-    <hr/>
-    <footer>
-      <p className="pull-right">
-        {t('this-site-in-other-languages')}
-        <a href="/?locale=en-US"> {t('languages.english')} </a>
-        <a href="/?locale=zh-CN"> {t('languages.simplified-chinese')} </a>
-        <a href="/?locale=zh-TW"> {t('languages.traditional_chinese')} </a>
-      </p>
-      <p>
-        &copy; 2016 Company, Inc.
-        &middot;
-        <a href="/?locale=en-US" target='_blank'> {t('languages.english')} </a>
-        &middot;
-        <a href="#">Terms</a>
-      </p>
-    </footer>
-  </div>
-)
+// import {refresh} from '../engines/auth/actions'
+
+const Widget = React.createClass({
+  componentDidMount () {
+    const {onRefresh} = this.props
+    onRefresh()
+  },
+  render () {
+    const {t} = this.props
+    return (
+      <div className="row">
+        <hr/>
+        <footer>
+          <p className="pull-right">
+            {t('this-site-in-other-languages')}
+            <a href="/?locale=en-US"> {t('languages.english')} </a>
+            <a href="/?locale=zh-CN"> {t('languages.simplified-chinese')} </a>
+            <a href="/?locale=zh-TW"> {t('languages.traditional_chinese')} </a>
+          </p>
+          <p>
+            &copy; 2016 Company, Inc.
+            &middot;
+            <a href="/?locale=en-US" target='_blank'> {t('languages.english')} </a>
+            &middot;
+            <a href="#">Terms</a>
+          </p>
+        </footer>
+      </div>
+    )
+  }
+})
 
 Widget.propTypes = {
-  t: PropTypes.func.isRequired
+  t: PropTypes.func.isRequired,
+  onRefresh: PropTypes.func.isRequired
 }
 
-export default translate()(Widget)
+const Model = connect(
+  state => ({info: state.siteInfo}),
+  dispatch => ({
+    onRefresh: function () {
+      console.log('refresh')
+      // ajax("get", "/site/info", null, function(ifo){
+      //   dispatch(refresh(ifo));
+      //   document.documentElement.lang = ifo.lang;
+      //   document.title = ifo.title;
+      // });
+    }
+  })
+)(Widget)
+
+export default translate()(Model)
