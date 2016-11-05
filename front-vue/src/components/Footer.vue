ae5748a14e5dae0c3c479ccbbfc68e4ad9c74b85
Jitang Zheng <jitang.zheng@gmail.com>
2016-11-05 13:58:16 -0700

    giveup i18next

--- front-vue/src/components/Footer.vue
+++ front-vue/src/components/Footer.vue
@@ -3,7 +3,11 @@
     <hr>
       <footer>
         <p class="float-xs-right"><a href="#">Back to top</a></p>
-        <p>&copy; {{ $t('attributes.body') }} &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        <p>
+          &copy; {{ $t('attributes.body') }}
+          &middot; <a href="#">Privacy</a>
+          &middot; <a href="#">Terms</a>
+        </p>
       </footer>
   </div>
 </template>
--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -2,11 +2,63 @@ require('./assets/main.css')
 require('./assets/reading.css')
 
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+Vue.use(VueI18n)
 
 import root from './engines'
-import i18n from './i18n'
 
-Vue.use(i18n)
+// var self = window.this
+// var lang = 'en-US'
+// Vue.locale(lang, function () {
+//   self.loading = true
+//   return window.fetch(process.env.API_HOST + '/locales/' + lang, {
+//     method: 'get',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     }
+//   }).then(function (res) {
+//     return res.json()
+//   }).then(function (json) {
+//     self.loading = false
+//     if (Object.keys(json).length === 0) {
+//       return Promise.reject(new Error('locale empty !!'))
+//     } else {
+//       return Promise.resolve(json)
+//     }
+//   }).catch(function (error) {
+//     self.error = error.message
+//     return Promise.reject()
+//   })
+// }, function () {
+//   Vue.config.lang = lang
+// })
+
+var lang = window.localStorage.getItem('locale') || 'zh-CN'
+
+Vue.locale(lang, function () {
+  return window.fetch(`${process.env.API_HOST}/locales/${lang}`, {
+    method: 'get',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }).then(function (res) {
+    return res.json()
+  }).then(function (json) {
+    if (Object.keys(json).length === 0) {
+      return Promise.reject(new Error('locale empty !!'))
+    } else {
+      return Promise.resolve(json)
+    }
+  }).catch(function (error) {
+    console.log(error.message)
+    return Promise.reject()
+  })
+}, function () {
+  Vue.config.lang = lang
+})
+
 /* eslint-disable no-new */
 new Vue({
   router: root.router
