fded8d6fffd6a580403660ee421d8825e117bbfb
Jitang Zheng <jitang.zheng@gmail.com>
2016-11-16 11:00:34 -0800

    shop methods and tables

--- db/postgres/migrations/20161115155850_InitShop.sql
+++ db/postgres/migrations/20161115155850_InitShop.sql
@@ -1,8 +1,223 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
+create table shop_products(
+  id serial primary key,
+  name varchar(255) not null,
+  description text not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create index idx_shop_products_name on shop_products(name);
 
+create table shop_variants(
+  id serial primary key,
+  sku varchar(255) not null,
+  price numeric(12,2) not null,
+  cost numeric(12,2) not null,
+  length numeric(12,2) not null,
+  weight numeric(12,2) not null,
+  height numeric(12,2) not null,
+  product_id int not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create unique index idx_shop_variants_sku on shop_variants(sku);
+
+create table shop_countries(
+  id serial primary key,
+  name varchar(255) not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create unique index idx_shop_countries_name on shop_countries(name);
+
+create table shop_states(
+  id serial primary key,
+  name varchar(255) not null,
+  country_id int not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create index idx_shop_states_name on shop_states(name);
+create unique index idx_shop_states_name_country on shop_states(name, country_id);
+
+create table shop_tax_rates(
+  id serial primary key,
+  value numeric(12,2) not null,
+  state_id int not null,
+  variant_id int not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create unique index idx_shop_tax_rates_state_variant on shop_tax_rates(state_id, variant_id);
+
+create table shop_properties(
+  id serial primary key,
+  name varchar(255) not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create unique index idx_shop_properties_name on shop_properties(name);
+
+create table shop_variants_properties(
+  id serial primary key,
+  variant_id int not null,
+  property_id int not null,
+  created_at timestamp without time zone not null default now()
+);
+create unique index idx_shop_variants_properties on shop_variants_properties(variant_id, property_id);
+
+create table shop_addresses(
+  id serial primary key,
+  firstname varchar(255) not null,
+  lastname varchar(255) not null,
+  address1 varchar(255) not null,
+  address2 varchar(255),
+  phone1 varchar(32) not null,
+  phone2 varchar(32),
+  zipcode char(6) not null,
+  company varchar(255),
+  state_id int not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+
+create table shop_orders(
+  id serial primary key,
+  uid varchar(255) not null,
+  item_total numeric(12,2) not null,
+  total numeric(12,2) not null,
+  adjustment_total numeric(12,2) not null,
+  payment_total numeric(12,2) not null,
+  state varchar(16) not null,
+  shipment_state varchar(16) not null,
+  payment_state varchar(16) not null,
+  user_id int not null,
+  completed_at timestamp without time zone,
+  address_id int not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create unique index idx_shop_orders_uid on shop_orders(uid);
+create index idx_shop_orders_state on shop_orders(state);
+create index idx_shop_orders_shipment_state on shop_orders(shipment_state);
+create index idx_shop_orders_payment_state on shop_orders(payment_state);
+
+create table shop_line_items(
+  id serial primary key,
+  variant_id int not null,
+  order_id int not null,
+  quantity int not null,
+  price numeric(8,2) not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create unique index idx_shop_line_items_variant_order on shop_line_items(variant_id, order_id);
+
+create table shop_payment_methods(
+  id serial primary key,
+  type varchar(16) not null,
+  name varchar(255) not null,
+  description text,
+  active boolean not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create index idx_shop_payment_methods_type on shop_payment_methods(type);
+create unique index idx_shop_payment_methods_name on shop_payment_methods(name);
+
+create table shop_payments(
+  id serial primary key,
+  amount numeric(8,2) not null,
+  order_id int not null,
+  payment_method_id int not null,
+  state varchar(16) not null,
+  response_code varchar(255) not null,
+  avs_response text not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create index idx_shop_payments_state on shop_payments(state);
+
+create table shop_shipment_methods(
+  id serial primary key,
+  name varchar(255) not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create unique index idx_shop_shipment_methods_name on shop_shipment_methods(name);
+
+create table shop_shipments(
+  id serial primary key,
+  tracking varchar(255) not null,
+  uid varchar(255) not null,
+  cost numeric(8,2) not null,
+  shipped_at timestamp without time zone,
+  order_id int not null,
+  shipping_method_id int not null,
+  address_id int not null,
+  state varchar(16) not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create unique index idx_shop_shipments_uid on shop_shipments(uid);
+create index idx_shop_shipments_state on shop_shipments(state);
+
+create table shop_return_authorizations(
+  id serial primary key,
+  uid varchar(255) not null,
+  state varchar(16) not null,
+  amount numeric(8,2) not null,
+  order_id int not null,
+  reason text not null,
+  enter_by_id int not null,
+  enter_at timestamp without time zone,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create unique index idx_shop_return_authorizations_uid on shop_return_authorizations(uid);
+create index idx_shop_return_authorizations_state on shop_return_authorizations(state);
+
+create table shop_inventory_units(
+  id serial primary key,
+  lock_version int not null,
+  state varchar(16) not null,
+  variant_id int not null,
+  shipment_id int not null,
+  return_authorization_id int not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create index idx_shop_inventory_units_state on shop_inventory_units(state);
+
+create table shop_chargebacks(
+  id serial primary key,
+  state varchar(16) not null,
+  order_id int not null,
+  operator_id int not null,
+  amount numeric(8,2) not null,
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone not null
+);
+create index idx_shop_chargebacks_state on shop_chargebacks(state);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
-
+drop table shop_chargebacks;
+drop table shop_inventory_units;
+drop table shop_return_authorizations;
+drop table shop_shipments;
+drop table shop_shipment_methods;
+drop table shop_payments;
+drop table shop_payment_methods;
+drop table shop_line_items;
+drop table shop_orders;
+drop table shop_addresses;
+drop table shop_variants_properties;
+drop table shop_properties;
+drop table shop_tax_rates;
+drop table shop_states;
+drop table shop_countries;
+drop table shop_variants;
+drop table shop_products;
--- engines/shop/models.go
+++ engines/shop/models.go
@@ -1,15 +1,60 @@
 package shop
 
+/**
+https://baya.github.io/2015/09/17/%E7%94%B5%E5%AD%90%E5%95%86%E5%8A%A1%E7%B3%BB%E7%BB%9F%E5%9F%BA%E7%A1%80%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%E5%92%8C%E6%B5%81%E7%A8%8B.html
+*/
+
 import (
+	"time"
+
+	"github.com/jinzhu/gorm"
 	"github.com/kapmahc/lotus/engines/auth"
 	"github.com/kapmahc/lotus/web"
 )
+
+//Product product
+type Product struct {
+	web.Model
+
+	Name        string `json:"name"`
+	Description string `json:"description"`
+
+	Variants []Variant `json:"variants"`
+}
+
+//TableName table name
+func (p *Product) TableName() string {
+	return "shop_products"
+}
+
+//Variant variant
+type Variant struct {
+	web.Model
+
+	SKU       string  `json:"sku"`
+	Weight    float64 `json:"weight"`
+	Height    float64 `json:"height"`
+	Width     float64 `json:"width"`
+	Length    float64 `json:"length"`
+	CostPrice float64 `json:"cost_price"`
+	Price     float64 `json:"price"`
+
+	ProductID uint       `json:"product_id"`
+	Product   Product    `json:"product"`
+	Poperties []Property `json:"properties"`
+}
+
+//TableName table name
+func (p *Variant) TableName() string {
+	return "shop_variants"
+}
 
 //Country country
 type Country struct {
 	web.Model
 
-	Name string
+	Name   string  `json:"name"`
+	States []State `json:"states"`
 }
 
 //TableName table name
@@ -32,32 +77,18 @@ func (p *State) TableName() string {
 	return "shop_states"
 }
 
-//TaxRate TaxRate
-type TaxRate struct {
-	web.Model
-
-	Value float64 `json:"value"`
-
-	StateID   uint   `json:"state_id"`
-	State     string `json:"state"`
-	ProductID uint   `json:"product_id"`
-	Product   string `json:"product"`
-}
-
-//TableName table name
-func (p *TaxRate) TableName() string {
-	return "shop_tax_rates"
-}
-
 //Address address
 type Address struct {
 	web.Model
 
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
-	Content   string `json:"content"`
+	Address1  string `json:"address1"`
+	Address2  string `json:"address2"`
 	Zip       string `json:"zip"`
-	Phone     string `json:"phone"`
+	Phone1    string `json:"phone1"`
+	Phone2    string `json:"phone2"`
+	Company   string `json:"company"`
 
 	UserID  uint      `json:"user_id"`
 	User    auth.User `json:"user"`
@@ -70,12 +101,31 @@ func (p *Address) TableName() string {
 	return "shop_addresses"
 }
 
+//TaxRate TaxRate
+type TaxRate struct {
+	web.Model
+
+	Value float64 `json:"value"`
+
+	StateID   uint   `json:"state_id"`
+	State     string `json:"state"`
+	VariantID uint   `json:"variant_id"`
+	Variant   string `json:"variant"`
+}
+
+//TableName table name
+func (p *TaxRate) TableName() string {
+	return "shop_tax_rates"
+}
+
 //Property property
 type Property struct {
 	web.Model
 
-	Name    string `json:"name"`
-	Content string `json:"content"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+
+	Variants []Variant `json:"variants"`
 }
 
 //TableName table name
@@ -83,39 +133,29 @@ func (p *Property) TableName() string {
 	return "shop_properties"
 }
 
-//Product product
-type Product struct {
-	web.Model
-}
-
-//TableName table name
-func (p *Product) TableName() string {
-	return "shop_products"
-}
-
-//Option product option
-type Option struct {
-	web.Model
-}
-
-//TableName table name
-func (p *Option) TableName() string {
-	return "shop_options"
-}
-
-//Promotion promotion
-type Promotion struct {
-	web.Model
-}
-
-//TableName table name
-func (p *Promotion) TableName() string {
-	return "shop_promotions"
-}
-
 //Order order
 type Order struct {
 	web.Model
+
+	UID             string  `json:"uid"`
+	ItemTotal       float64 `json:"item_total"`
+	Total           float64 `json:"total"`
+	AdjustmentTotal float64 `json:"adjustment_total"`
+	PaymentTotal    float64 `json:"payment_total"`
+
+	//:cart, :address, :delivery, :payment, :confirm, :complete
+	State string `json:"state"`
+	//:ready, :pending, :partial, :shipped, :backorder, :canceled
+	ShipmentState string `json:"shipment_state"`
+	//:balance_due, :paid, :credit_owed, :failed, :void
+	PaymentState string `json:"payment_state"`
+
+	CompletedAt *time.Time `json:"completed_at"`
+
+	AddressID uint    `json:"address_id"`
+	Address   Address `json:"address"`
+	User      string  `json:"user"`
+	UserID    uint    `json:"user_id"`
 }
 
 //TableName table name
@@ -123,9 +163,53 @@ func (p *Order) TableName() string {
 	return "shop_orders"
 }
 
+//LineItem line item
+type LineItem struct {
+	web.Model
+
+	Quantity uint    `json:"quantity"`
+	Price    float64 `json:"price"`
+
+	VariantID uint    `json:"variant_id"`
+	Variant   Variant `json:"variant"`
+	OrderID   uint    `json:"order_id"`
+	Order     Order   `json:"order"`
+}
+
+//TableName table name
+func (p *LineItem) TableName() string {
+	return "shop_line_items"
+}
+
+//PaymentMethod payment method
+type PaymentMethod struct {
+	web.Model
+
+	Type        string `json:"type"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Active      bool   `json:"active"`
+}
+
+//TableName table name
+func (p *PaymentMethod) TableName() string {
+	return "shop_payment_methods"
+}
+
 //Payment payment
 type Payment struct {
 	web.Model
+
+	Amount float64 `json:"amount"`
+	// :checkout, :pending, :processing, :failed, :completed
+	State        string `json:"state"`
+	ResponseCode string `json:"response_code"`
+	AvsResponse  string `json:"avs_response"`
+
+	OrderID         uint          `json:"order_id"`
+	Order           Order         `json:"order"`
+	PaymentMethodID uint          `json:"payment_method_id"`
+	PaymentMethod   PaymentMethod `json:"payment_method"`
 }
 
 //TableName table name
@@ -133,32 +217,103 @@ func (p *Payment) TableName() string {
 	return "shop_payments"
 }
 
-//Refund refund
-type Refund struct {
-	web.Model
+//ShipmentMethod shipment method
+type ShipmentMethod struct {
+	gorm.Model
+
+	Name string `json:"name"`
 }
 
 //TableName table name
-func (p *Refund) TableName() string {
-	return "shop_refunds"
-}
-
-//Price price
-type Price struct {
-	web.Model
-}
-
-//TableName table name
-func (p *Price) TableName() string {
-	return "shop_price"
+func (p *ShipmentMethod) TableName() string {
+	return "shop_shipment_methods"
 }
 
 //Shipment shipment
 type Shipment struct {
 	web.Model
+
+	Tracking  string     `json:"tracking"`
+	UID       string     `json:"uid"`
+	Cost      float64    `json:"cost"`
+	ShippedAt *time.Time `json:"shipped_at"`
+	//:ready, :pending, :assemble, :cancelled, :shipped
+	State string `json:"state"`
+
+	OrderID         uint          `json:"order_id"`
+	Order           Order         `json:"order"`
+	PaymentMethodID uint          `json:"payment_method_id"`
+	PaymentMethod   PaymentMethod `json:"payment_method"`
 }
 
 //TableName table name
 func (p *Shipment) TableName() string {
 	return "shop_shipments"
+}
+
+//ReturnAuthorization return authorization
+type ReturnAuthorization struct {
+	web.Model
+	UID     string     `json:"uid"`
+	State   string     `json:"state"`
+	Amount  float64    `json:"amount"`
+	Reason  string     `json:"reason"`
+	EnterAt *time.Time `json:"enter_at"`
+
+	EnterByID uint       `json:"enter_by_id"`
+	EnterBy   *auth.User `json:"enter_by"`
+	OrderID   uint       `json:"order_id"`
+	Order     Order      `json:"order_id"`
+}
+
+//TableName table name
+func (p *ReturnAuthorization) TableName() string {
+	return "shop_return_authorizations"
+}
+
+//InventoryUnit inventory unit
+type InventoryUnit struct {
+	web.Model
+
+	LockVersion int    `json:"lock_version"`
+	State       string `json:"state"`
+
+	VariantID             uint `json:"variant_id"`
+	OrderID               uint `json:"order_id"`
+	ShipmentID            uint `json:"shipment_id"`
+	ReturnAuthorizationID uint `json:"return_authorization_id"`
+}
+
+//TableName table name
+func (p *InventoryUnit) TableName() string {
+	return "shop_inventory_units"
+}
+
+//Chargeback chargeback
+type Chargeback struct {
+	web.Model
+
+	State string `json:"state"`
+
+	OrderID    uint    `json:"order_id"`
+	OperatorID uint    `json:"operator_id"`
+	Amount     float64 `json:"amount"`
+}
+
+//TableName table name
+func (p *Chargeback) TableName() string {
+	return "shop_chargebacks"
+}
+
+//Promotion promotion
+type Promotion struct {
+	web.Model
+
+	Type   string `json:"type"`
+	Script string `json:"script"`
+}
+
+//TableName table name
+func (p *Promotion) TableName() string {
+	return "shop_promotions"
 }
